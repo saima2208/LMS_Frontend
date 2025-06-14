1e161211a0f363494609abdf65aabeb940286145
saima <saimasonya22@gmail.com>
2025-06-14 18:33:10 +0600

    feedback & contact done

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -3,80 +3,43 @@ import { Contact } from '../../model/contact.model';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ContactService } from '../services/contact.service';
+
+
 
 @Component({
   selector: 'app-contact',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-// contacts:Contact = new Contact();
-// isEditMode:boolean =true;
-
-
-//  constructor(private router: Router) {
-//     const nav = this.router.getCurrentNavigation();
-//     if (nav?.extras.state && nav.extras.state['contact']) {
-//       this.contacts = nav.extras.state['contact'];
-//       debugger;
-
-//   }
-//   }
-
-//   ngOnInit(): void {
-//    // Check if editing an existing course
-//     const state = history.state;
-//    if (state && state.contact) {
-//       this.contacts = state.contact;
-//      //  this.isEditMode = true;
-//     }
-//   }
-
-//   saveContact(): void {
-//    const contacts = JSON.parse(localStorage.getItem('contacts') || '[]');
-
-//    if (this.isEditMode) {
-//      // Update existing course
-//      const index = contacts.findIndex((p: Contact) => p.id === this.contacts.id);
-//      if (index !== -1) {
-//        contacts[index] = this.contacts;
-//      }
-//    } else {
-//      // Add new course
-//      this.contacts.id = contacts.length > 0 ? Math.max(...contacts.map((p: Contact) => p.id)) + 1 : 1;
-//      contacts.push(this.contacts);
-//    }
-
-//    // Save back to localStorage
-//    localStorage.setItem('contacts', JSON.stringify(contacts));
-//    this.router.navigate(['/faq']); // Navigate back to course list
-//  }
-
-
-contact: Contact = {
-    name: '',
-    email: '',
-    message: ''
-  };
-
-  constructor(private router: Router) {}
-
+  contacts: Contact = new Contact();
+  // contact: Contact[] = [];
+  constructor(private router: Router,
+     private contactService: ContactService
+    // private toastr: ToastrService
+  ) { }
   saveContact(): void {
-    // Retrieve existing contacts from localStorage
-    const contacts: Contact[] = JSON.parse(localStorage.getItem('contacts') || '[]');
-
-    // Add new contact to the array
-    contacts.push(this.contact);
-
-    // Save updated array back to localStorage
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-    alert("Message sent Successfully")
-
-    // Clear form
-    this.contact = { name: '', email: '', message: '' };
-
-    // Navigate to FAQ page
-    // this.router.navigate(['/faq']);
+    this.addContact();
   }
+  addContact(): void {
+    this.contactService.addContact(this.contacts).subscribe({
+      next: () => {
+        // this.toastr.success("Message sent successfully");
+        alert('Message sent successfull');
+        this.router.navigate(['/faq']);
+        this.resetForm();
+      },
+      error: (err) => {
+        // this.toastr.error('Failed to submit message' + err.message);
+        alert('Failed to submit message' + err.message);
+        console.error(err);
+      },
+    });
+  }
+  resetForm() {
+    this.contacts = new Contact();
+  }
+
 }
---
+++ src/app/pages/services/contact.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Contact } from '../../model/contact.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+   private apiUrl = 'http://localhost:8080/api/contact';
+
+  constructor(private http: HttpClient) {}
+
+  addContact(contact:Contact): Observable<Contact> {
+    return this.http.post<Contact>(this.apiUrl,contact,{
+      headers: new HttpHeaders({ 'Content-Type':'application/json'}),
+    });
+  }
+
+  getAllMessages(): Observable<Contact[]>{
+return this.http.get<Contact[]>(`${this.apiUrl}`);
+  }
+}
